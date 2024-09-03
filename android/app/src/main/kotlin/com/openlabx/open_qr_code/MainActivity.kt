afb69850ce79d03abf63d8ba7527a0a91f7fcafc
Kataranjee <93165947+ajee10x@users.noreply.github.com>
2024-09-03 13:49:12 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/openlabx/open_qr_code/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.openlabx.open_qr_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/openlabx/open_qr_code/old_MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.open_qr_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
